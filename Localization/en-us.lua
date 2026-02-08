971747750539cc96b7b38a8422b2da28833a455d
Mod-Of-Theseus <ModOfTheseus@gmail.com>
2026-02-08 11:47:33 -0600

    Removed Residual
    Oops

--- Localization/en-us.lua
+++ Localization/en-us.lua
@@ -535,16 +535,6 @@ return {
 
     },
 
-      j_mot_fireiceJ = {
-        name = "FireIce",
-        text = {
-          "Gain {X:dark_edition,C:white}^1{} Mult for each used {X:spectral,C:white}Spectral{},",
-          "{X:dark_edition,C:white}^0.25{} Mult for each used {X:tarot,C:white}Tarot{} or {X:planet,C:white}Planet{}.",
-          "{C:inactive}Additional {X:dark_edition,C:white}^0.25{}{C:inactive} if {X:dark,C:white}Deteriorated{}.",
-          "{C:inactive}Currently: {X:dark_edition,C:white}^#1#{}"
-        },
-      },
-
     Spectral = {
       c_mot_bermuda = {
         name = "Bermuda",
